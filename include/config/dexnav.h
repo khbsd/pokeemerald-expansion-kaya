0a59e1586d07b8b2279fe57c033f2ba0e72346c9
Ken Bynell <ken.bynell@gmail.com>
2025-01-08 02:31:57 -0600

    configure dexnav, clean up save

--- include/config/dexnav.h
+++ include/config/dexnav.h
@@ -2,14 +2,14 @@
 #define GUARD_CONFIG_DEXNAV_H
 
 #define DEXNAV_ENABLED                TRUE  // Whether or not DexNav is enabled. If TRUE, flags/vars below must all be non-zero
-#define USE_DEXNAV_SEARCH_LEVELS      FALSE  /* WARNING: POSSIBLY EXCEEDS SAVEBLOCK SPACE! REQUIRES 1 BYTE PER SPECIES */
+#define USE_DEXNAV_SEARCH_LEVELS      TRUE  /* WARNING: POSSIBLY EXCEEDS SAVEBLOCK SPACE! REQUIRES 1 BYTE PER SPECIES */
 
 // Flag/var defines
-#define FLAG_SYS_DEXNAV_SEARCH          0 // Searching for mon
+#define FLAG_SYS_DEXNAV_SEARCH          1 // Searching for mon
 #define FLAG_SYS_DEXNAV_GET             0 // DexNav shows in start menu
-#define FLAG_SYS_DETECTOR_MODE          0 // Allow player to find hidden mons
-#define VAR_DEXNAV_SPECIES              0 // Registered DexNav species
-#define VAR_DEXNAV_STEP_COUNTER         0 // Steps for finding hidden pokemon
+#define FLAG_SYS_DETECTOR_MODE          1 // Allow player to find hidden mons
+#define VAR_DEXNAV_SPECIES              600 // Registered DexNav species
+#define VAR_DEXNAV_STEP_COUNTER         HIDDEN_MON_STEP_COUNT // Steps for finding hidden pokemon
 
 // Search parameters
 #define DEXNAV_TIMEOUT                  15  // 15 seconds is the time out. Max of 1092 seconds allowed
