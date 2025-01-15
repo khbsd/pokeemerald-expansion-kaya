a67f40da2dcaf31c2acfdfea3579b0eb837d9746
khbsd <khb.dev@gmail.com>
2025-01-15 00:28:21 -0600

    im too tired too be coding

--- src/data/abilities.h
+++ src/data/abilities.h
@@ -2429,7 +2429,7 @@ const struct Ability gAbilitiesInfo[ABILITIES_COUNT] =
         .description = COMPOUND_STRING("Normal moves become Dark."),
         .aiRating = 8,
     },
-    [ABILITY_OBSCURATE] =
+    [ABILITY_CAPILLARATE] =
     {
         .name = _("Capillarate"),
         .description = COMPOUND_STRING("Normal moves become Fighting."),
