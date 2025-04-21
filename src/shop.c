7bccc369029450af0428590a29b84dbbdfd6591b
khbsd <khb.dev@gmail.com>
2025-04-21 12:17:14 -0500

    fix: shop menu was glitched

--- src/shop.c
+++ src/shop.c
@@ -830,6 +830,7 @@ static void BuyMenuDrawMapMetatile(s16 x, s16 y, const u16 *src, u8 metatileLaye
     if (metatileLayerType == METATILE_LAYER_TYPE_NORMAL)
     {
         BuyMenuDrawMapMetatileLayer(sShopData->tilemapBuffers[2], offset1, offset2, src + 0);
+        BuyMenuDrawMapMetatileLayer(sShopData->tilemapBuffers[3], offset1, offset2, src + 4);
         BuyMenuDrawMapMetatileLayer(sShopData->tilemapBuffers[1], offset1, offset2, src + 8);
     }
     else
