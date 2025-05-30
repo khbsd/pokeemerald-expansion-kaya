ec75ff3d9126fc6618a3e749c4dd94449993a15c
khbsd <khb.dev@gmail.com>
2025-05-30 14:55:32 -0500

    fix: formatting

--- src/battle_util.c
+++ src/battle_util.c
@@ -4194,8 +4194,7 @@ enum Abilities AbilityBattleEffects(u32 caseID, u32 battler, enum Abilities abil
                 break;
             case ABILITY_PANACEA:
                 gBattleScripting.battler = BATTLE_PARTNER(battler);
-                if (!IsBattlerAtMaxHp(battler)
-                    && !(gStatuses3[battler] & STATUS3_HEAL_BLOCK))
+                if (!IsBattlerAtMaxHp(battler) && !(gStatuses3[battler] & STATUS3_HEAL_BLOCK))
                 {
                     BattleScriptPushCursorAndCallback(BattleScript_IceBodyHeal);
                     gBattleStruct->moveDamage[battler] = GetNonDynamaxMaxHP(battler) / 32;
