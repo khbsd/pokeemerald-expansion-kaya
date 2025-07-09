571ece0be5e0b30b3a9084c10510082eaa350a43
khbsd <khb.dev@gmail.com>
2025-07-09 04:34:14 -0500

    fix: better formatting ok bye

--- src/pokemon_summary_screen.c
+++ src/pokemon_summary_screen.c
@@ -552,7 +552,7 @@ static const struct WindowTemplate sSummaryTemplate[] =
     },
     [PSS_LABEL_WINDOW_PROMPT_RELEARN] = {
         .bg = 0,
-        .tilemapLeft = 19,
+        .tilemapLeft = 18,
         .tilemapTop = 2,
         .width = 11,
         .height = 2,
