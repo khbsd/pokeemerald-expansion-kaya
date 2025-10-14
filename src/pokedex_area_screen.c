157d550fa18e6015008ba7bb06787ea869b6fae0
khbsd <khb.dev@gmail.com>
2025-10-14 14:50:52 -0500

    update: cleanup and script now updates define in include/pokedex.h

--- src/pokedex_area_screen.c
+++ src/pokedex_area_screen.c
@@ -342,21 +342,21 @@ static void FindMapsWithMon(u16 species)
     }
 
     // Add regular species to the area map
-    for (i = 0; i < MAX_LOCATIONS; i++)
+    for (u32 id = 0; id < MAX_LOCATIONS; id++)
     {
-        u32 headerId = gPokemonDexLocationIds[species][gAreaTimeOfDay][i];
-        DebugPrintf("%u", i);
-        DebugPrintf("%u", headerId);
-        if (MAP_GROUP(headerId) != MAP_GROUP(MAP_UNDEFINED))
+        u32 headerId = gPokemonDexLocationIds[species][gAreaTimeOfDay][id];
+        u32 mapGroup = MAP_GROUP(headerId);
+        u32 mapNum   = MAP_NUM(headerId);
+        if (mapGroup != MAP_GROUP(MAP_UNDEFINED))
         {
-            switch (MAP_GROUP(headerId))
+            switch (mapGroup)
             {
             case MAP_GROUP_TOWNS_AND_ROUTES:
-                SetAreaHasMon(MAP_GROUP(headerId), MAP_NUM(headerId));
+                SetAreaHasMon(mapGroup, mapNum);
                 break;
             case MAP_GROUP_DUNGEONS:
             case MAP_GROUP_SPECIAL_AREA:
-                SetSpecialMapHasMon(MAP_GROUP(headerId), MAP_NUM(headerId));
+                SetSpecialMapHasMon(mapGroup, mapNum);
                 break;
             }
         }
