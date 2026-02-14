91c1ffa3354959002b35fede589e09ae76f17761
khbsd <khb.dev@gmail.com>
2026-02-14 03:54:13 -0600

    code(pokedex): never treat nat dex as enabled

--- src/event_data.c
+++ src/event_data.c
@@ -122,10 +122,7 @@ void EnableNationalPokedex(void)
 
 bool32 IsNationalPokedexEnabled(void)
 {
-    if (gSaveBlock2Ptr->pokedex.nationalMagic == 0xDA && VarGet(VAR_NATIONAL_DEX) == 0x302 && FlagGet(FLAG_SYS_NATIONAL_DEX))
-        return TRUE;
-    else
-        return FALSE;
+    return FALSE;
 }
 
 void DisableMysteryEvent(void)
